77cc0cec25fba0d117ae123ccc3e8455781d42de
nnzhuilian <609117168@qq.com>
2018-12-14 10:57:44 +0800

    up

---
+++ src/Algorithm/sortex/MoreThanHalfNum_Solution.java
@@ -0,0 +1,61 @@
+package Algorithm.sortex;
+
+import java.util.TreeMap;
+
+public class MoreThanHalfNum_Solution {
+	public int MoreThanHalfNum_Solution1(int [] array) {//基于TreeMap或HashMap的
+		int length=array.length;
+		if(length==0){
+			return 0;
+		}
+		if(length==1){
+			return array[0];
+		}
+        TreeMap<Integer,Integer> tm=new TreeMap<Integer,Integer>();
+        for(int i=0;i<length;i++){
+            if(tm.containsKey(array[i])){
+            	int num=tm.get(array[i])+1;
+            	if(num>length/2){
+            		return array[i];
+            	}
+            	tm.put(array[i], num);
+            }else{
+            	tm.put(array[i], 1);
+            }
+        }
+        return 0;
+    }
+	
+    public int MoreThanHalfNum_Solution2(int [] array) {//方法2，相同+1，不同-1的方法
+        if(array.length==0){
+            return 0;
+        }
+        int[] cal=new int[2];
+        int length=array.length;
+        for(int i=0;i<length;i++){
+            if(array[i]!=cal[0]){
+                if(cal[1]>0){
+                    cal[1]--;
+                }
+                if(cal[1]==0){
+                    cal[0]=array[i];
+                    cal[1]=1;
+                }
+            }
+            else if(array[i]==cal[0]){
+                cal[1]++;
+            }
+        }
+        int count=0;
+        for(int i=0;i<length;i++){
+            if(array[i]==cal[0]){
+                count++;
+            }
+        }
+         if(count>(length/2)){
+             return cal[0];
+         }
+         return 0;
+     }
+    //方法3 基于partition。
+}
---
+++ src/Algorithm/sortex/Permutation.java
@@ -0,0 +1,69 @@
+package Algorithm.sortex;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class Permutation {
+	public ArrayList<String> Permutation1(String str) {
+        ArrayList<String> strs=new ArrayList<String>();
+        if(str.length()==0){
+            return strs;
+        }
+         
+       ArrayList<Character> strr=new ArrayList<Character>();
+        for(int i=0;i<str.length();i++){
+            strr.add(str.charAt(i));
+        }
+        TreeSet<String> tr=new TreeSet<String>();
+        StringBuffer sb=new StringBuffer();
+        sort(tr,strr,sb);
+        Iterator<String> it=tr.iterator();
+        while(it.hasNext()){
+            strs.add(it.next());
+        }
+        return strs;
+    }
+    public void sort(TreeSet<String> strs,ArrayList<Character> strr,StringBuffer sb){
+        if(strr.size()==0){
+            strs.add(sb.toString());
+            return;
+        }
+        for(int i=0;i<strr.size();i++){
+            ArrayList<Character> temp=new ArrayList<Character>(strr);
+            StringBuffer s=new StringBuffer(sb.toString());
+            s.append(strr.get(i));
+            temp.remove(i);
+            sort(strs,temp,s);
+        }
+    }
+ ///////////////////////////-优化方法-////////////////////////////   
+    ArrayList<String> res = new ArrayList<String>();
+    public ArrayList<String> Permutation(String str) {
+        if(str == null)
+            return res;
+        PermutationHelper(str.toCharArray(), 0);
+        Collections.sort(res);
+        return res;
+    }
+    public void PermutationHelper(char[] str, int i){
+        if(i == str.length - 1){
+            res.add(String.valueOf(str));
+        }else{
+            for(int j = i; j < str.length; j++){
+                if(j!=i && str[i] == str[j])
+                    continue;
+                swap(str, i, j);
+                PermutationHelper(str, i+1);
+                swap(str, i, j);
+            }
+        }
+    }
+    public void swap(char[] str, int i, int j) {
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+
+}
